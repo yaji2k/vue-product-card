78484b6b57a8b6128b4144e3fc28d6def8749d51
Timur <yaji2k@gmail.com>
2019-04-27 15:32:01 +0300

    cleaning project

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-// import Home from './views/Home.vue';
+
 import Login from './views/Login.vue';
 import Register from './views/Register.vue';
 import Products from './views/Products.vue';
@@ -24,14 +24,14 @@ export default new Router({
       component: Product,
       props: true,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // },
     {
       path: '/login',
       name: 'login',
